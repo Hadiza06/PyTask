2d573e1404775bd2d98e3ddb284bd9bb99221f76
Hadiza06 <hadiza9013@gmail.com>
2025-10-29 16:12:42 +0100

    Jour 4: Implémentation supprimer_mission avec gestion d'erreurs

--- task.py
+++ task.py
@@ -8,10 +8,10 @@ class TaskMaster:
             "terminee": False
         }
         self.missions.append(dic)
-        # Tu peux enlever le 'pass' ici
+       
     
     def marquer_terminee(self, numero_mission):
-        # Correction : accéder à la mission par son index
+       # accéder à la mission par son index
         if 1 <= numero_mission <= len(self.missions):
             self.missions[numero_mission - 1]["terminee"] = True
         else:
@@ -24,6 +24,25 @@ class TaskMaster:
             for i, mission in enumerate(self.missions, start=1):
                 statut = "✅" if mission["terminee"] else "⏳"
                 print(f"Tâche à accomplir : {i}. {mission['description']} [{statut}]")
+
+    def supprimer_mission(self, numero_mission):
+        if 1<=numero_mission<=len(self.missions):
+            mission_supprimee=self.missions.pop(numero_mission - 1)
+            print(f"🗑️ Mission '{mission_supprimee['description']}' supprimée !")
+        else:
+            print(f"❌ Mission {numero_mission} n'existe pas!")
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     tm = TaskMaster()
@@ -39,3 +58,16 @@ if __name__ == "__main__":
     print("\n--- Test marquer comme terminée ---")
     tm.marquer_terminee(1)  # Marquer la première mission
     tm.afficher_missions()
+
+    print("\n--- Test suppression mission ---")
+    print("Avant suppression :")
+    tm.afficher_missions()
+    
+    tm.supprimer_mission(1)  # Supprimer "Apprendre Python"
+    print("\nAprès suppression :")
+    tm.afficher_missions()
+    
+    print("\n--- Test suppression mission inexistante ---")
+    tm.supprimer_mission(10)  # Doit afficher erreur
+
+
